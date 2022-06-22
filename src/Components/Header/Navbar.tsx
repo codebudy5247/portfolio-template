492c19e8379b2b3e05a3518403923bac7be463e7
codebuddy5247 <aditya@imentus.com>
2022-06-22 11:23:18 +0530

    :tada:

--- src/Components/Header/Navbar.tsx
+++ src/Components/Header/Navbar.tsx
@@ -57,7 +57,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            {/* PORTFOLIO */}
+            PORTFOLIO
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
